98e14178d2c15009de9a52a8732aa19d0527f989
Calacly <54817827+Calacly@users.noreply.github.com>
2020-04-18 11:29:36 +0200

    First Commit

---
+++ circleClass.js
@@ -0,0 +1,74 @@
+// CREATE CANVAS - SAME WIDTH AS VIEWPORT
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//CREATE CIRCLE VARIABLES 
+let x = Math.floor(Math.random() * innerWidth) ;
+let y = Math.floor(Math.random() * innerHeight);
+let speed = (Math.random() - 0.5) * 10;
+let dx = speed;
+let dy = speed;
+let radius = 30;
+
+console.log(x)
+console.log(y)
+console.log(radius)
+//CREATE CIRCLE 
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function() {
+        
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = "blue";
+        c.stroke();
+    }
+
+    
+
+    this.update = function() {
+        //Bounce logic:
+    if(this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+        this.dx = -this.dx;
+    } 
+
+    if(this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+        this.dy = -this.dy;
+    } 
+
+
+        this.x += this.dx;
+        this.y += this.dy;
+        this.draw()
+    };
+    
+   
+}
+
+let circle = new Circle(x,y,dx,dy,radius);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight)
+
+    circle.update()
+
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "blue";
+    c.stroke();
+
+}
+
+
+
+animate()
+
